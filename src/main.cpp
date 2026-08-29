5928f726e67427914e01352f3f3b3123bd455a4a
Kasi-ab <kasia15b6@gmail.com>
2026-08-29 13:43:44 +0530

    Add safety module

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include "../include/planning_problem.h"
 #include "../include/graph.h"
+#include "../include/safety.h"
+#include <iostream>
 
 int main() {
-    PlanningProblem p;
-    Graph g(p);
+    State a; a.id = 1; a.embedding = {0.0, 0.0};
+    State bad; bad.id = 2; bad.embedding = {3.0, 4.0};
+
+    std::vector<State> allStates = {a, bad};
+    std::vector<uint64_t> badIds = {2};
+
+    double d = distanceToNearestBadState(a, badIds, allStates);
+    std::cout << "Distance: " << d << std::endl; // should print 5
+
     return 0;
 }
