bfdb077c97d29a6086bfb3607390cda8c96e8908
Kasi-ab <kasia15b6@gmail.com>
2026-08-29 14:04:27 +0530

    Add heuristic module

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include "../include/planning_problem.h"
 #include "../include/graph.h"
 #include "../include/safety.h"
+#include "../include/heuristic.h"
 #include <iostream>
 
 int main() {
-    State a; a.id = 1; a.embedding = {0.0, 0.0};
-    State bad; bad.id = 2; bad.embedding = {3.0, 4.0};
+    State s; s.id = 1; s.embedding = {0.0, 0.0};
+    State goal; goal.id = 2; goal.embedding = {3.0, 4.0};
 
-    std::vector<State> allStates = {a, bad};
-    std::vector<uint64_t> badIds = {2};
-
-    double d = distanceToNearestBadState(a, badIds, allStates);
-    std::cout << "Distance: " << d << std::endl; // should print 5
+    double h = heuristic(s, goal);
+    std::cout << "Heuristic: " << h << std::endl; // should print 5
 
     return 0;
 }
